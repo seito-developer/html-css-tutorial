4b39b24c3dafaeca335e6ad0bf0cbf7365028b94
seito <seito.horiguchi@gmail.com>
2022-01-01 14:31:15 +0900

    js OK

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// Smooth scroll
+const $doc = document;
+const $nav = $doc.getElementById('js-nav');
+const $navLink = $nav.getElementsByTagName('a');
+
+for (var i = $navLink.length - 1; i >= 0; i--) {
+	$navLink[i].addEventListener('click', (e) => {
+		e.preventDefault()
+		const $this = e.currentTarget;
+		const targetName = $this.getAttribute('href').replace('#', '');
+		const $target = $doc.getElementById(targetName);
+		scroll({
+			top: $target.offsetTop - 50,
+		    behavior: "smooth"
+		 });
+	})
+}
