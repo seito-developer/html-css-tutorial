5f6da3b495602c8c36edfbaa7d1e62d483dc8ce3
seito <seito.horiguchi@gmail.com>
2022-01-03 01:11:56 +0900

    comp

--- 3.website/assets/js/app.js
+++ 3.website/assets/js/app.js
@@ -1,22 +1,5 @@
-// Smooth scroll
-const $doc = document;
-const $nav = $doc.getElementById('js-nav');
-const $navLink = $nav.getElementsByTagName('a');
-
-for (var i = $navLink.length - 1; i >= 0; i--) {
-	$navLink[i].addEventListener('click', (e) => {
-		e.preventDefault()
-		const $this = e.currentTarget;
-		const targetName = $this.getAttribute('href').replace('#', '');
-		const $target = $doc.getElementById(targetName);
-		scroll({
-			top: $target.offsetTop - 50,
-		    behavior: "smooth"
-		 });
-	});
-}
-
-// particles
-particlesJS.load('js-particles', './assets/js/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
 });
+
+
